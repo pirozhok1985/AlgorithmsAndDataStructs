a0d16e66fe1dbf5abd36d8f919a6b82bcaf5d0c9
pirozhok <pirozhok1985@gmail.com>
2021-04-06 20:28:07 +1000

    Максимальное число из 4-х

--- Lesson1/Source.c
+++ Lesson1/Source.c
@@ -6,10 +6,23 @@ double IMT(double h, double m);
 
 int main(int argc, char* argv[])
 {
+	//-------------------IMT-------------------------
 	double height, weight;
 	printf("Enter your height(in meters) and weight using coma as delimeter: ");
 	scanf("%lf, %lf", &height, &weight);
 	printf("Your IMT = %.0lf", IMT(height, weight));
+	//---------------------------------------------
+	//---------------------Max value---------------
+	int a, b, c, d;
+	int max, max1, max2;
+	printf("\nEnter four numbers using coma as delimeter: ");
+	scanf("%d, %d, %d, %d", &a, &b, &c, &d);
+	max1 = (a > b) ? a : b;
+	max2 = (c > d) ? c : d;
+	max = (max1 > max2) ? max1 : max2;
+	printf("Max number is %d", max);
+	//---------------------------------------------
+
 	return 0;
 }
 
