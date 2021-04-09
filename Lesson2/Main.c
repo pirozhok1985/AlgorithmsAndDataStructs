0887ea04ebde0fed6074c807af00d3c18f9d778f
Pirozhok1985 <pirozhok1985@gmail.com>
2021-04-09 19:03:06 +1000

    Оформление

--- Lesson2/Main.c
+++ Lesson2/Main.c
@@ -5,15 +5,18 @@
 void ConvertDToB(int number);
 int myPow(int number, int degree);
 int myPowRecursion(int number, int degree);
+int myPowFastRecursion(int number, int degree);
 int main(int argc, char* argv[])
 {
-	//int num;
-	//printf("Enter the decimal number to convert: ");
-	//scanf("%d", &num);
-	//printf("Converted binary number: ");
-	//ConvertDToB(num);
-
-	printf("%d", myPowRecursion(2, 10));
+	int num;
+	printf("Enter the decimal number to convert: ");
+	scanf("%d", &num);
+	printf("Converted binary number: ");
+	ConvertDToB(num);
+	int myNumber, pow;
+	printf("\nEnter the number and power using coma as delim: ");
+	scanf("%d, %d", &myNumber, &pow);
+	printf("Result using myPow: %d\nResult using myPowRecursion: %d\nResult using myPowFastRecursion: %d", myPow(myNumber, pow), myPowRecursion(myNumber, pow), myPowFastRecursion(myNumber, pow));
 	return 0;
 }
 void ConvertDToB(int number)
@@ -35,9 +38,20 @@ int myPow(int number, int degree)
 }
 int myPowRecursion(int number, int degree)
 { 
-	--degree;
-	int result = 1;
-	if (degree > 0)
-		result = myPowRecursion(number, degree);
-	return result *= number;
+	return degree == 1 ? number : number * myPowRecursion(number, degree - 1);
+}
+int myPowFastRecursion(int number, int degree)
+{
+	if (degree == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		if (degree % 2 == 0)
+		{
+			return number * number * myPowFastRecursion(number, degree - 2);
+		}
+		return number * myPowFastRecursion(number, degree - 1);
+	}
 }
