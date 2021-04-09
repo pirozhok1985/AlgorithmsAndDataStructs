3c19fe2b56a1457e1fd6f2d0d7299170ee457610
Pirozhok1985 <pirozhok1985@gmail.com>
2021-04-09 11:52:24 +1000

    Реализовал функцию возведения в степень

--- Lesson2/Main.c
+++ Lesson2/Main.c
@@ -3,13 +3,16 @@
 #include <math.h>
 
 void ConvertDToB(int number);
+int myPow(int number, int degree);
 int main(int argc, char* argv[])
 {
-	int num;
-	printf("Enter the decimal number to convert: ");
-	scanf("%d", &num);
-	printf("Converted binary number: ");
-	ConvertDToB(num);
+	//int num;
+	//printf("Enter the decimal number to convert: ");
+	//scanf("%d", &num);
+	//printf("Converted binary number: ");
+	//ConvertDToB(num);
+
+	printf("%d", myPow(2, 5));
 	return 0;
 }
 void ConvertDToB(int number)
@@ -19,4 +22,13 @@ void ConvertDToB(int number)
 		ConvertDToB(number / 2);
 	}
 	printf("%d", number%2);
+}
+int myPow(int number, int degree)
+{
+	int result = number;
+	for (int i = 0; i < degree - 1; i++)
+	{
+		result *= number;
+	}
+	return result;
 }
