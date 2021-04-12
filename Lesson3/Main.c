cf38f8f0dd37b3c0ae2ea5db5a43f0f236fa5480
Pirozhok1985 <pirozhok1985@gmail.com>
2021-04-12 15:04:46 +1000

    Оптимизировал bubble sort

--- Lesson3/Main.c
+++ Lesson3/Main.c
@@ -11,21 +11,45 @@ void Swap(int *x, int *y)
 int main(int argc, char* argv[])
 {
 	int myArr[ARRAY_LENGTH];
+	int compareCount = 0;
+	int min;
 	for (int i = 0; i < ARRAY_LENGTH; i++)
 	{
 		myArr[i] = rand() % 101;
 		printf("%d ", myArr[i]);
 	}
 	printf("\n\n\n");
+
 	//---------bubble sort-----------//
+	//for (int i = 0; i < ARRAY_LENGTH; i++)
+	//{
+	//	for (int j = 0; j < ARRAY_LENGTH - 1; j++)
+	//	{
+	//		if (myArr[j] > myArr[j + 1]) {
+	//			Swap(&myArr[j], &myArr[j + 1]);
+	//			compareCount++;
+	//		}
+	//	}
+	//}
+	
+
+	//---------bubble sort perfomance---------//
 	for (int i = 0; i < ARRAY_LENGTH; i++)
 	{
-		for (int j = 0; j < ARRAY_LENGTH - 1; j++)
+		min = i;
+		for (int j = i; j < ARRAY_LENGTH; j++)
 		{
-			if (myArr[j] > myArr[j + 1])
-				Swap(&myArr[j], &myArr[j + 1]);
+			if (myArr[j] < myArr[min]) {
+				min = j;
+				compareCount++;
+			}
 		}
+		if(myArr[min] < myArr[i])
+			Swap(&myArr[min], &myArr[i]);
 	}
+	printf("Compare count equals: %d\n", compareCount);
+
+
 	//---------Print sorted array-------//
 	for (int i = 0; i < ARRAY_LENGTH; i++)
 	{
