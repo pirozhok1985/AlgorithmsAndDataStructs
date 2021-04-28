30452bd54ca1cbaa7f6783ce77cd9f17d14626b8
Pirozhok1985 <pirozhok1985@gmail.com>
2021-04-28 14:12:00 +1000

    Реализовал программу считывания матрицы смежности из файла

--- Lesson7/Main.c
+++ Lesson7/Main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void PrintMatrix(char* fileName);
 
 int main(int argc, char** argv)
 {
+	PrintMatrix("matrix.txt");
 	return 0;
+}
+
+void PrintMatrix(char* fileName)
+{
+	FILE* fileStream = fopen(fileName, "rb");
+	if(fileStream == NULL)
+	{
+		printf("Open file error!");
+		exit(1);
+	}
+	fseek(fileStream, 0, SEEK_END);
+	long fsize = ftell(fileStream);
+	rewind(fileStream);
+
+	char* buffer = (char*)malloc(sizeof(char) * fsize);
+	if (buffer == NULL)
+	{
+		printf("Error buffer creation!");
+		exit(1);
+	}
+
+	fread(buffer, 1, fsize, fileStream);
+	puts(buffer);
+
+	fclose(fileStream);
+	free(buffer);
 }
